6782d984ed78ff5f12bb61b6ef191d6bbad447b2
Simbarashe Dzinamarira <simbadzina@apache.org>
2025-07-22 13:46:29 -0700

    Initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Welcome Gala for Students and Parents",
+        "excerpt":"We held an event.   Details and pictures  ","categories": ["Post Formats"],
+        "tags": ["event"],
+        "url": "/post%20formats/welcome-gala/",
+        "teaser": null
+      },{
+        "title": "Highlighting Sciency",
+        "excerpt":"               Sciency Learning       Zimbabwe            ","categories": ["Post Formats"],
+        "tags": ["Post Formats"],
+        "url": "/post%20formats/sciency-highlight/",
+        "teaser": null
+      },{
+        "title": "Nomaqhawe Ncube",
+        "excerpt":"I am a passionate public health professional with a Master of Public Health (MPH) from the University of Michigan – Ann Arbor, specializing in Social and Behavioral Health Sciences and certified in Social Epidemiology. My work focuses on advancing health equity, accessibility, and sustainable public health solutions. I bring hands-on...","categories": [],
+        "tags": [],
+        "url": "/students/nomaqhawe_ncube/",
+        "teaser": "/assets/images/students/noma_headshot.jpg"
+      },{
+        "title": "Simbarashe Dzinamarira",
+        "excerpt":"Simbarashe Dzinamarira is a PhD student in the Computer Science Department at Rice University. He is current working on distributed storage with Prof. Eugene Ng in the BOLD lab. Simbarashe holds an M.S. in Computer Science from Rice University and a B.Eng. in Computer Science from the Hong Kong University...","categories": [],
+        "tags": [],
+        "url": "/students/simbarashe_dzinamarira/",
+        "teaser": "/assets/images/students/simba_teaser.jpg"
+      }]
